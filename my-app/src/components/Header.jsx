07e2850eba63adac492f08f040fd5fbe40bba27a
john Doe <sataward666@gmail.com>
2022-07-26 10:31:05 +0200

    create app.js components, assets

---
+++ my-app/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import '../style/Header.css';
+
+
+function Header () {
+    return(
+    <div className="App-header">
+        
+        <p>Bienvenue sur l'App du Gamer</p>
+        
+    </div>
+    );
+}
+
+
+export default Header;
